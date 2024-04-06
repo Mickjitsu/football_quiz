cebc0aa14218aca8c6593c91eb310306774177d4
Michael McCourt <mmccourt928@outlook.com>
2024-04-06 08:34:20 +0000

    attach event listeners function defined for choice option in start page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     gameOpener();
-    /*attachEventListeners();*/
+    attachEventListeners();
 });
 
 /*This function opens a welcome page for 3 seconds before diverting to the start page*/
@@ -15,15 +15,36 @@ function gameOpener(){
         mainScreen.innerHTML = `<div class="start-menu" id="start-match">
         <p>Start Match</p>
     </div>
-    <div class="start-menu" id="difficulty">
+    <div class="start-menu" id="league">
         <p>Choose your league</p>
     </div>
     <div class="start-menu" id="difficulty">
-        <p>Choose your difficultu</p>
+        <p>Choose your difficulty</p>
     </div>
     <div class="start-menu" id="rules">
         <p>Tutorial</p>
     </div>`}
         
     },3000);
+    attachEventListeners()
 }
+
+function attachEventListeners(){
+    const start = document.querySelector('#start-match');
+    const difficulty = document.querySelector('#difficulty');
+    const league = document.querySelector('#league');
+    const rules = document.querySelector('#rules');
+
+    if(start){
+        start.addEventListener('click', startGame)
+    };
+    if(difficulty){
+        difficulty.addEventListener('click', diffChoice);
+    }
+    if(league){
+        league.addEventListener('click', leagueChoice)
+    };
+    if(rules){
+        rules.addEventListener('click', rulesPage)
+    };
+};
