2ac74954461e7a65ceb362beacf3e3fdcf349bd5
Michael McCourt <mmccourt928@outlook.com>
2024-04-06 09:02:17 +0000

    event listener added to log users league choice, and new game opener function added to remove 3 second wait for main screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,14 @@ document.addEventListener('DOMContentLoaded', function(){
     attachEventListeners();
 });
 
+var mainScreen = document.querySelector('.main-screen')
 /*This function opens a welcome page for 3 seconds before diverting to the start page*/
 function gameOpener(){
     setTimeout(function(){
         var welcomeDiv = document.querySelector('.welcome');
         if (welcomeDiv) welcomeDiv.classList.add('hidden');
 
-        var mainScreen = document.querySelector('.main-screen');
+
         if(mainScreen){mainScreen.classList.remove('hidden');
 
         mainScreen.innerHTML = `<div class="start-menu" id="start-match">
@@ -23,28 +24,121 @@ function gameOpener(){
     </div>
     <div class="start-menu" id="rules">
         <p>Tutorial</p>
-    </div>`}
-        
+    </div>`
+
+    attachEventListeners();
+}
+    
     },3000);
-    attachEventListeners()
+}
+
+/*this allows users to be able to return to the main screen without a 3 second delay*/
+function gameOpener2(){
+    if(mainScreen){mainScreen.classList.remove('hidden');
+
+        mainScreen.innerHTML = `<div class="start-menu" id="start-match">
+        <p>Start Match</p>
+    </div>
+    <div class="start-menu" id="league">
+        <p>Choose your league</p>
+    </div>
+    <div class="start-menu" id="difficulty">
+        <p>Choose your difficulty</p>
+    </div>
+    <div class="start-menu" id="rules">
+        <p>Tutorial</p>
+    </div>`
+
+    attachEventListeners();
+}
 }
 
 function attachEventListeners(){
-    const start = document.querySelector('#start-match');
-    const difficulty = document.querySelector('#difficulty');
-    const league = document.querySelector('#league');
-    const rules = document.querySelector('#rules');
+    const startDiv = document.querySelector('#start-match');
+    const difficultyDiv = document.querySelector('#difficulty');
+    const leagueDiv = document.querySelector('#league');
+    const rulesDiv = document.querySelector('#rules');
 
-    if(start){
-        start.addEventListener('click', startGame)
+    
+    if(startDiv){
+        startDiv.addEventListener('click', startGame)
     };
-    if(difficulty){
-        difficulty.addEventListener('click', diffChoice);
+    if(difficultyDiv){
+        difficultyDiv.addEventListener('click', diffChoice)
     }
-    if(league){
-        league.addEventListener('click', leagueChoice)
+    if(leagueDiv){
+        leagueDiv.addEventListener('click', leagueChoice)
     };
-    if(rules){
-        rules.addEventListener('click', rulesPage)
+    if(rulesDiv){
+        rulesDiv.addEventListener('click', rulesPage)
     };
+
+};
+
+function leagueChoice(){
+    mainScreen.innerHTML = `<div class="start-menu leagues" id="premier-league">
+    <p>Premier League</p>
+    </div>
+    <div class="start-menu leagues" id="scottish-league">
+    <p>Scottish League</p>
+    </div>
+    <div class="start-menu leagues" id="la-liga">
+    <p>La Liga</p>
+    </div>`
+    attachLeagueListener()
+}
+/*the functions below have placeholder text for the time being*/
+function startGame(){
+    mainScreen.innerHTML = `<div class="start-menu leagues" id="premier-league">
+    <p>Premier League</p>
+    </div>
+    <div class="start-menu leagues" id="scottish-league">
+    <p>Scottish League</p>
+    </div>
+    <div class="start-menu leagues" id="la-liga">
+    <p>La Liga</p>
+    </div>`
+}
+
+function diffChoice(){
+    mainScreen.innerHTML = `<div class="start-menu leagues" id="premier-league">
+    <p>Premier League</p>
+    </div>
+    <div class="start-menu leagues" id="scottish-league">
+    <p>Scottish League</p>
+    </div>
+    <div class="start-menu leagues" id="la-liga">
+    <p>La Liga</p>
+    </div>`
+}
+function rulesPage(){
+    mainScreen.innerHTML = `<div class="start-menu leagues" id="premier-league">
+    <p>Premier League</p>
+    </div>
+    <div class="start-menu leagues" id="scottish-league">
+    <p>Scottish League</p>
+    </div>
+    <div class="start-menu leagues" id="la-liga">
+    <p>La Liga</p>
+    </div>`
+}
+
+
+/*this function does not have placeholder text. This will determine the league choice by the user*/
+function attachLeagueListener(){
+    document.querySelector('#premier-league').addEventListener('click', function(){
+        league = 'premier league';
+        console.log('The league is set to the Premier League')
+        gameOpener2()
+    });
+    document.querySelector('#scottish-league').addEventListener('click', function(){
+        league = 'scottish league';
+        console.log('The league is set to the scottish premiership')
+        gameOpener2()
+    });
+    document.querySelector('#la-liga').addEventListener('click', function(){
+        league = 'La liga';
+        console.log('The league is set to the Spanish La Liga')
+        gameOpener2()
+    });
 };
