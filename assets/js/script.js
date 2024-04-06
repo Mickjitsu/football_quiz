ed3c53261e0bff707e466d6163da08832a8f4c32
Michael McCourt <mmccourt928@outlook.com>
2024-04-06 08:25:28 +0000

    event listener and function added for a welcome page that times out before the start page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
     gameOpener();
-    attachEventListeners();
+    /*attachEventListeners();*/
 });
+
+/*This function opens a welcome page for 3 seconds before diverting to the start page*/
+function gameOpener(){
+    setTimeout(function(){
+        var welcomeDiv = document.querySelector('.welcome');
+        if (welcomeDiv) welcomeDiv.classList.add('hidden');
+
+        var mainScreen = document.querySelector('.main-screen');
+        if(mainScreen){mainScreen.classList.remove('hidden');
+
+        mainScreen.innerHTML = `<div class="start-menu" id="start-match">
+        <p>Start Match</p>
+    </div>
+    <div class="start-menu" id="difficulty">
+        <p>Choose your belt</p>
+    </div>
+    <div class="start-menu" id="rules">
+        <p>Tutorial</p>
+    </div>`}
+        
+    },3000);
+}
