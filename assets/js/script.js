efe33bcf82e6d56ce9fe90c1fcf583ce27bef22a
Michael McCourt <mmccourt928@outlook.com>
2024-04-06 09:17:45 +0000

    function added to log users difficulty choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,27 +110,18 @@ function startGame(){
 }
 
 function diffChoice(){
-    mainScreen.innerHTML = `<div class="start-menu leagues" id="premier-league">
-    <p>Premier League</p>
+    mainScreen.innerHTML = `<div class="start-menu difficulty" id="easy">
+    <p>The League Cup</p>
     </div>
-    <div class="start-menu leagues" id="scottish-league">
-    <p>Scottish League</p>
+    <div class="start-menu difficulty" id="medium">
+    <p>The league</p>
     </div>
-    <div class="start-menu leagues" id="la-liga">
-    <p>La Liga</p>
+    <div class="start-menu difficulty" id="hard">
+    <p>The treble</p>
     </div>`
+    attachDiffListener();
 }
-function rulesPage(){
-    mainScreen.innerHTML = `<div class="start-menu leagues" id="premier-league">
-    <p>Premier League</p>
-    </div>
-    <div class="start-menu leagues" id="scottish-league">
-    <p>Scottish League</p>
-    </div>
-    <div class="start-menu leagues" id="la-liga">
-    <p>La Liga</p>
-    </div>`
-}
+
 
 
 /*this function does not have placeholder text. This will determine the league choice by the user*/
@@ -151,3 +142,22 @@ function attachLeagueListener(){
         gameOpener2()
     });
 };
+
+/*this determines and logs the users difficulty choice*/
+function attachDiffListener(){
+    document.querySelector('#easy').addEventListener('click', function(){
+        difficulty = 'easy';
+        console.log('Difficulty is set to easy')
+        gameOpener2()
+    });
+    document.querySelector('#medium').addEventListener('click', function(){
+        difficulty = 'medium';
+        console.log('Difficulty is set to medium')
+        gameOpener2()
+    });
+    document.querySelector('#hard').addEventListener('click', function(){
+        difficulty = 'hard';
+        console.log('Difficulty is set to hard')
+        gameOpener2()
+    })
+}
